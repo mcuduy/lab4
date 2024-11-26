a6a7f5e861675598e3dd4ca35cc66accae7fa988
ĐẶNG TRẦN ANH DUY <duy.dangduytv0601@hcmut.edu.vn>
2024-11-26 11:04:51 +0700

    Add files via upload

---
+++ Core/Inc/global.h
@@ -0,0 +1,22 @@
+/*
+ * global.h
+ *
+ *  Created on: Nov 26, 2024
+ *      Author: ADMIN
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include "main.h"
+#include "scherduler.h"
+#include <stdint.h>
+
+void led_red_toggle();
+void led_orange_toggle();
+void led_yellow_toggle();
+void led_green_toggle();
+void led_blue_toggle();
+
+
+#endif /* INC_GLOBAL_H_ */
